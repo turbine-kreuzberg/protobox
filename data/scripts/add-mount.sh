51eada44e251e8be20d5cebac9e9dcb7a6c2a503
Bernd Alter <bernd.alter@votum.de>
2014-07-20 21:50:08 +0200

    new role 'myproject' for custom project settings, scripts etc.

---
+++ data/scripts/add-mount.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+MNT_DIR="/mnt/mybook_media"
+
+[ -d  "$MNT_DIR" ] || mkdir "$MNT_DIR"
+
+CHECK=$( grep "$MNT_DIR" /etc/fstab )
+
+if [ -z "$CHECK" ]; then
+
+    NEW_MOUNT="192.168.1.220:/volume1/mybook/shop/media $MNT_DIR nfs rw,nfsvers=3 0 0"
+
+    echo "$NEW_MOUNT" >> /etc/fstab
+
+    mount -a
+fi
